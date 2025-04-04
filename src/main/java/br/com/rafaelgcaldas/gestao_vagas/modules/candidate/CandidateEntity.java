af05ec0d0180c5f3bce0b4d0498399b5e5d2763a
Rafael Gomes <rafael.gcaldas01@gmail.com>
2025-04-03 22:40:57 -0300

    chore: init project

---
+++ src/main/java/br/com/rafaelgcaldas/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,40 @@
+package br.com.rafaelgcaldas.gestao_vagas.modules.candidate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name="candidate")
+public class CandidateEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
+    private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido")
+    private String email;
+
+    @Length(min = 3, max = 12, message = "A senha deve conter entre 3 e 12 caracteres")
+    private String password;
+
+    private String description;
+    private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
