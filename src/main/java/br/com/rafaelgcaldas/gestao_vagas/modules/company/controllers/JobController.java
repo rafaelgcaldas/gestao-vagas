db601271299a75dd80c0880ea4c537bd86781578
Rafael Gomes <rafael.gcaldas01@gmail.com>
2025-04-07 23:55:29 -0300

    feat: add job controller

---
+++ src/main/java/br/com/rafaelgcaldas/gestao_vagas/modules/company/controllers/JobController.java
@@ -0,0 +1,24 @@
+package br.com.rafaelgcaldas.gestao_vagas.modules.company.controllers;
+
+import br.com.rafaelgcaldas.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.rafaelgcaldas.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/job")
+public class JobController {
+
+    @Autowired
+    private CreateJobUseCase createJobUseCase;
+
+    @PostMapping("/")
+    public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
+        return this.createJobUseCase.execute(jobEntity);
+    }
+
+}
