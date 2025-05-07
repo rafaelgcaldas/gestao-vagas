8a7dffe806736bfb99de0f89cb7c2482a65e2874
Rafael Gomes <rafael.gcaldas01@gmail.com>
2025-05-06 22:43:33 -0300

    chore: route public config

--- src/main/java/br/com/rafaelgcaldas/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/rafaelgcaldas/gestao_vagas/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.rafaelgcaldas.gestao_vagas.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,7 +11,14 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/candidate/").permitAll()
+                            .requestMatchers("/company/").permitAll();
+
+                    auth.anyRequest().authenticated();
+
+                });
         return http.build();
     }
 }
