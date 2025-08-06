86c071537783591bcf9dde407c069b1773e2645e
Rafael Gomes <rafael.gcaldas01@gmail.com>
2025-08-05 23:08:23 -0300

    chore: swagger config

--- src/main/java/br/com/rafaelgcaldas/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/rafaelgcaldas/gestao_vagas/GestaoVagasApplication.java
@@ -1,9 +1,18 @@
 package br.com.rafaelgcaldas.gestao_vagas;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Gestão de Vagas API",
+		version = "1.0.0",
+		description = "API para gestão de vagas de emprego"
+	)
+)
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
