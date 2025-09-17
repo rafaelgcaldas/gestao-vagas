5e20a1f5f238cb21a682c77be14ffbc4fcade345
Rafael Gomes <rafael.gcaldas01@gmail.com>
2025-09-16 23:41:07 -0300

    chore: Security scheme config

--- src/main/java/br/com/rafaelgcaldas/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/rafaelgcaldas/gestao_vagas/GestaoVagasApplication.java
@@ -1,7 +1,10 @@
 package br.com.rafaelgcaldas.gestao_vagas;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,6 +15,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 		version = "1.0.0",
 		description = "API para gestão de vagas de emprego"
 	)
+)
+@SecurityScheme(
+		name = "jwt_auth",
+		scheme = "bearer",
+		type = SecuritySchemeType.HTTP,
+		bearerFormat = "JWT",
+		in = SecuritySchemeIn.HEADER
 )
 public class GestaoVagasApplication {
 
