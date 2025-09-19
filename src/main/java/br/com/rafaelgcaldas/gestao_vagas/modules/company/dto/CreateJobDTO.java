5bc98343e6cc97ba6f72064fce18bd5576e2380e
Rafael Gomes <rafael.gcaldas01@gmail.com>
2025-09-18 22:39:14 -0300

    chore: adding request example

--- src/main/java/br/com/rafaelgcaldas/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/rafaelgcaldas/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,11 +1,17 @@
 package br.com.rafaelgcaldas.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
 
-    private String description;
-    private String benefits;
-    private String level;
+  @Schema(example = "Vaga para Desenvolvedor Java Pleno", requiredMode = Schema.RequiredMode.REQUIRED)
+  private String description;
+
+  @Schema(example = "GYMPAss, Plano de saúde...", requiredMode = Schema.RequiredMode.REQUIRED)
+  private String benefits;
+
+  @Schema(example = "JUNIOR, PLENO, SENIOR", requiredMode = Schema.RequiredMode.REQUIRED)
+  private String level;
 }
